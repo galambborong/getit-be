7acf9d0f2dfae3c53527d1be1cdbdf7f78b9fbbc
Vitalie <vitalie1998@hotmail.com>
2021-03-08 13:02:29 +0000

    users table created

---
+++ db/migrations/20210308124920_create_users_table.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex) {
+  console.log('Creating users table');
+  return knex.schema.createTable('users', (usersTable) => {
+    usersTable.string('username').primary()
+    usersTable.string('avatar_url').notNullable();
+    usersTable.string('name').notNullable();
+  });
+};
+
+exports.down = function(knex) {
+    console.log('Dropping users table')
+    return knex.schema.dropTable('users')
+};
