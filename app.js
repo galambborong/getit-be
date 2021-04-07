f1f57001ad7d4e62461748c730588e95cebf6d61
Peter Keenan <keenan@posteo.net>
2021-04-07 08:54:39 +0100

    Enable CORS requests

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 const express = require('express');
+const cors = require('cors');
 const apiRouter = require('./routes/apiRouter');
 const {
-  err500,
   customErrorHandler,
-  err405,
   err400,
-  err404
+  err404,
+  err500
 } = require('./errors/index');
+
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/api', apiRouter);
-app.use(customErrorHandler);
 
+app.use(customErrorHandler);
 app.use(err400);
 app.use(err404);
-// app.use(err405);
 app.use(err500);
 
 module.exports = app;
