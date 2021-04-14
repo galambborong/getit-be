8f964d6450586141f4f9052ee7464ffbb2470fdf
Peter Keenan <keenan@posteo.net>
2021-04-14 17:48:32 +0100

    Refactor util functions

--- db/utils/data-manipulation.js
+++ db/utils/data-manipulation.js
@@ -1,31 +1,27 @@
 // extract any functions you are using to manipulate your data, into this file
 
 exports.modifyTimeStamp = (array) => {
-  const newArray = array.map((article) => {
-    const newArt = { ...article };
+  return array.map((article) => {
+    const newArt = {...article};
     const toConvert = article['created_at'];
     const dateTime = new Date(toConvert);
     newArt.created_at = dateTime.toISOString();
     return newArt;
   });
-
-  return newArray;
 };
 
 exports.createRefObj = (array, key, value) => {
   const newObj = {};
   array.map((item) => {
     const newKey = item[key];
-    const newValue = item[value];
-    newObj[newKey] = newValue;
+    newObj[newKey] = item[value];
   });
-
   return newObj;
 };
 
 exports.formatItems = (array, key, value, refObj) => {
-  const formattedItem = array.map((item) => {
-    const copyItem = { ...item };
+  return array.map((item) => {
+    const copyItem = {...item};
     const temporaryValue = copyItem[key];
     if (refObj) {
       copyItem[value] = refObj[temporaryValue];
@@ -35,5 +31,4 @@ exports.formatItems = (array, key, value, refObj) => {
     delete copyItem[key];
     return copyItem;
   });
-  return formattedItem;
 };
